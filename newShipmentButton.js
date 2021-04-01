3a340c6145da1882958346248d9d60ad299776cc
Marvin Bolanos <46733575+HarmonicVoid@users.noreply.github.com>
2021-03-31 21:17:55 -0700

    Created a new alert for the user to avoid spams and to confirm the current shipment is done. Also made some format and spelling corrections.
    The user cannot add a new shipment until all display stock has been used and DOAs reported.

--- newShipmentButton.js
+++ newShipmentButton.js
@@ -1,25 +1,68 @@
- // IMPORTANT: Test your code before adding the dialog. Dialogs cannot run in the editor. It runs ONLY when user action is present from sheets.
-
-var spreadsheet = SpreadsheetApp.getActive();
-var displayTackerSheet = spreadsheet.setActiveSheet(spreadsheet.getSheetByName('Pixel Display Tracker'), true);
-
+var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+var displayTrackerSheet = spreadsheet.getSheetByName("menues and elements");
 // This is the key to finding the last row of your sheet. Use logger to see what your last row is.
-var lastRow = displayTackerSheet.getLastRow();
+var lastRow = displayTrackerSheet.getLastRow();
 
-// This is the main function linked to the Button. This runs the show. 
+// **This is the main function linked to the Button. This runs the show**
 function buttonPressed() {
-  showAlert();
+  displaysVerification();
 }; 
 
-// Created an alert for the user to avoid accidental clicks or spams.
-function showAlert() {
+// This is where the magic happens. Due to my template, I had to come up with some workarounds to make it work.
+function addNewShipment() {  
+
+  // Since it did not paste on the right row, I made the 'var nextRowToPaste' and adding 2 solved the issue for me.
+  var nextRowToPaste = lastRow + 2;
+  var firstGroupSelection = nextRowToPaste;
+  var secondGroupSelection = firstGroupSelection + 4;
+  
+  // To group the cells, the 'getRange()' method required a string in this formart --> "20:22"
+  var beginGrouping = firstGroupSelection.toString() + ":" + secondGroupSelection.toString();
+
+  // Make sure to use loggers to see what number the method 'lastRow();' gives you.
+  // IMPORTANT: Test your code before adding the dialog. Dialogs cannot run in the editor. It runs ONLY when user action is present from sheets.
+  Logger.log("First Group = " + firstGroupSelection);
+  Logger.log("Second Group = " + secondGroupSelection);
+  Logger.log("beginGrouping = " + beginGrouping);
+
+  displayTrackerSheet.getRange('A' + nextRowToPaste).activate();
+
+  spreadsheet.getRange('\'<--Template\'!B2:K8').copyTo(spreadsheet.getActiveRange(), SpreadsheetApp.CopyPasteType.PASTE_NORMAL, false);
+    
+  // What goes in these methods depends on how and what you are pasting.
+  displayTrackerSheet.getRange(beginGrouping).activate();
+  displayTrackerSheet.setCurrentCell(spreadsheet.getRange('A' + nextRowToPaste));
+  displayTrackerSheet.getActiveRange().shiftRowGroupDepth(1);
+  displayTrackerSheet.getRowGroup(secondGroupSelection + 1, 1).collapse();
+
+  Logger.log("Updated last row number" + nextRowToPaste);
+}
+
+/* Created an alert for the user to avoid spams and to confirm the current shipment is done.
+  * The user cannot add a new shipment until all display stock has been used and DOAs reported.
+*/ 
+function displaysVerification() {
+
+  var displayStockValue = displayTrackerSheet.getRange('H' + lastRow).getValue();
+  var doaReportChecked = displayTrackerSheet.getRange('J' + lastRow).getDisplayValue();
+  var countedFor = displayTrackerSheet.getRange('E' + lastRow).getDisplayValue();
+
+  if (doaReportChecked == "FALSE" || doaReportChecked == "TRUE" && displayStockValue != 0 && countedFor >= 5) {
+    cannotAddShipmentAlert();
+    displayTrackerSheet.getRange('J' + lastRow).setValue("FALSE")
+  } else {
+      confirmAddShipmentAlert();
+    }
+}
+
+// Created an alert for the user to confirm adding a new shipment after 'displaysVerification();' checks current shipment is done.
+function confirmAddShipmentAlert() {
   var ui = SpreadsheetApp.getUi();
   var result = ui.alert(
      'Please confirm',
      'Are you sure you want to add a new shipment?',
       ui.ButtonSet.YES_NO);
 
-  // Process the user's response.
   if (result == ui.Button.YES) {
     ui.alert('Adding shipment!');
     // if yes is clicked, the function that automates the pasting and grouping is called.
@@ -29,35 +72,8 @@ function showAlert() {
   }
 }
 
-// This is were the magic happens. Due to my template, I had to come up with some workarounds to make it work.
-function addNewShipment() {
-  
-  // 2 was added because the last row number was 2 rows off. It did not paste on the right row, adding 2 solved the issue for me
-  var nextRowToPaste = lastRow + 2;
-  var firstGroupSelection = nextRowToPaste;
-  var secondGroupSelection = firstGroupSelection + 2;
-  
-  // To group the cells, the 'getRange()' method required a string in this formart --> "20:22"
-  var beginGrouping = firstGroupSelection.toString() + ":" + secondGroupSelection.toString();
-
-  // Make sure to use loggers to see what last row number the method gives you.
-  // IMPORTANT: Test your code before adding the dialog. Dialogs cannot run in the editor. It runs ONLY when user action is present from sheets.
-    Logger.log(nextRowToPaste);
-    Logger.log("First Group = " + firstGroupSelection);
-    Logger.log("Second Group = " + secondGroupSelection);
-    Logger.log("beginGrouping = " + beginGrouping);
-
-    displayTackerSheet.getRange('A' + nextRowToPaste).activate();
-
-    spreadsheet.getRange('\'<--Template\'!B2:K6').copyTo(spreadsheet.getActiveRange(), SpreadsheetApp.CopyPasteType.PASTE_NORMAL, false);
-    
-  // What goes in the methods depends on how and what you are pasting.
-    displayTackerSheet.getRange(beginGrouping).activate();
-    displayTackerSheet.setCurrentCell(spreadsheet.getRange('A' + nextRowToPaste));
-    displayTackerSheet.getActiveRange().shiftRowGroupDepth(1);
-    displayTackerSheet.getRowGroup(secondGroupSelection + 1, 1).collapse();
-
-    Logger.log("Updated last row number" + nextRowToPaste);
+function cannotAddShipmentAlert() {
+  var ui = SpreadsheetApp.getUi();
+  ui.alert('Cannot add shipment','Please use all the display stock and make sure DOAs are reported.', ui.ButtonSet.OK);
 }
-
 
